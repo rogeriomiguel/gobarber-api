0c756e12ae2d6108607a6d6ec73969fd8bff0b40
Rogerio Miguel <rogerio.miguel1@hotmail.com>
2020-04-27 10:44:25 -0300

    Models loader

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -4,7 +4,7 @@ class User extends Model {
   static init(sequelize) {
     super.init(
       {
-        name: sequelize.STRING,
+        name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
