46f605fc6c3bf7a767f92f88fa330c101657eba2
Rogerio Miguel <rogerio.miguel1@hotmail.com>
2020-04-29 15:09:21 -0300

    Nodemailer config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6b6121fcde1fa6',
+    pass: '14f2ff36784190',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
